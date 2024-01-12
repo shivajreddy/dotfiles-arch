5d5ac92bc886bc49f78b89da00100fef9453e3a8
Shiva <shivajreddy@outlook.com>
2024-01-11 21:02:46 -0500

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,14 +21,39 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Plugins - load
 local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
+
+-- Options - setup
 local opts = {}
 
 require("lazy").setup(plugins, opts)
 
+-- configure tree-sitter plugin
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = ({"html", "javascript", "typescript", "vim", "lua", "python"}),
+  sync_install = false,
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+vim.keymap.set('n', '<leader>n', ':noh<CR>')
+
 -- Set up theme
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
 
