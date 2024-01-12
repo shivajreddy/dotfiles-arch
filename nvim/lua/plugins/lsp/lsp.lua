22ba17b6152a6f87bfe0f85a08d14ef82183954c
Shiva <shivajreddy@outlook.com>
2024-01-12 15:46:27 -0500

    refactored nvim configuration. CLEAN now

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -1,28 +1,23 @@
 -- This file setups multiple plugins used for setting up LSP's
 
 return {
-	-- 1. This is the plugin used to download lsp servers
-	-- https://github.com/williamboman/mason.nvim?tab=readme-ov-file#installation
+	-- 1. Plugin to download lsp servers, formatters, linters
 	{
 		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
 		end,
 	},
-	-- 2. this plugin allows to install lsp server via mason from below settings
-	-- this eliminates for us to manuall open :Mason and then install the lsp servers
-	-- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#installation
+	-- 2. Install packages via mason from below settings
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright" }, -- install these language servers
+				ensure_installed = { "lua_ls", "pyright" },
 			})
 		end,
 	},
-	-- 3. Nvim-lspconfig : this allows our neovim to communicate with the lsp servers
-	-- that are installed and running on our system
-	-- https://github.com/neovim/nvim-lspconfig
+	-- 3. Nvim-lspconfig :: neovim to communicate with our lsp servers
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
