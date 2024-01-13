c7d99dc183b528ccf298c05b10906620f9409a32
Shiva <shivajreddy@outlook.com>
2024-01-12 22:06:33 -0500

    .

--- nvim/lua/plugins/auto-complete/nvim-cmp.lua
+++ nvim/lua/plugins/auto-complete/nvim-cmp.lua
@@ -1,13 +1,18 @@
 return {
-	"hrsh7th/nvim-cmp",
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
+	},
+	{ "hrsh7th/nvim-cmp" },
 	config = function()
+		require("luasnip.loaders.from_vscode").lazy_load()
 		local cmp = require("cmp")
 		cmp.setup({
 			snippet = {
 				-- REQUIRED - you must specify a snippet engine
 				expand = function(args)
 					vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-					require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
 					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
 				end,
@@ -21,16 +26,13 @@ return {
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
 				["<C-e>"] = cmp.mapping.abort(),
-        -- Accept currently selected item.
-        -- Set `select` to `false` to only confirm explicitly selected items.
+				-- Accept currently selected item.
+				-- Set `select` to `false` to only confirm explicitly selected items.
 				["<CR>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "vsnip" }, -- For vsnip users.
-				-- { name = 'luasnip' }, -- For luasnip users.
-				-- { name = 'ultisnips' }, -- For ultisnips users.
-				-- { name = 'snippy' }, -- For snippy users.
+				-- { name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users.
 			}, {
 				{ name = "buffer" },
 			}),
